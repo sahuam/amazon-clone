0b411d314f165018798258dceab003097dd68746
sahuam <amrit.sahu10.95@gmial.com>
2020-10-30 14:20:21 +0530

    firebase config added

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCwc2Vqxsfw9TqTqJYuc67s0WBMRb5zNZ8",
-    authDomain: "clone-8e7f7.firebaseapp.com",
-    databaseURL: "https://clone-8e7f7.firebaseio.com",
-    projectId: "clone-8e7f7",
-    storageBucket: "clone-8e7f7.appspot.com",
-    messagingSenderId: "813686976206",
-    appId: "1:813686976206:web:dce397537ce05d9913ea9f"
-  };
+  apiKey: "AIzaSyDiRZc6Zc0IQOq_odnscD0blIgo34Z4h4E",
+  authDomain: "clone-577a5.firebaseapp.com",
+  databaseURL: "https://clone-577a5.firebaseio.com",
+  projectId: "clone-577a5",
+  storageBucket: "clone-577a5.appspot.com",
+  messagingSenderId: "105496029416",
+  appId: "1:105496029416:web:6b19b97bb0a51d59a3815d"
+};
 
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
